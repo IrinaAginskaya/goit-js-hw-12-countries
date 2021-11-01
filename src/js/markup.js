5ba5558050303dc3708b35a33b78791215b173e9
IrinaAginskaya <aginska@gmail.com>
2021-11-01 16:16:21 +0200

    Red2

--- src/js/markup.js
+++ src/js/markup.js
@@ -5,42 +5,32 @@ import countriesListTpl from '../templates/countries.hbs';
 import pnotify from './pnotify';
 
 const debounce = require('lodash.debounce');
-const arrorMessage = 'Too many matches found. Please enter a more specific query!';
+const errorMessage = 'Too many matches found. Please enter a more specific query!';
 
-const resultMoreTen = array => {
-  if (array.length > 10) {
-    refs.result.innerHTML = '';
-    pnotify(arrorMessage);
-  } else {
-    return array;
-  }
-};
-
-const resultMoreOne = array => {
-  if (array && array.length > 1 && array.length < 10) {
-    markupСountriesList(array);
-  } else {
-    return array;
-  }
-};
-
-const markupСountriesList = array => {
-  refs.result.innerHTML = countriesListTpl(array);
-};
-
-const createsResultTpl = array => {
-  if (array) {
-    refs.result.innerHTML = countryCardTpl(array[0]);
-  }
-};
-
-const markupCountryResult = e => {
-  const value = e.target.value.trim();
-  if (!value) {
-    refs.result.innerHTML = '';
-    return;
-  }
-  fetchCountries(value).then(resultMoreTen).then(resultMoreOne).then(createsResultTpl);
-};
 
 refs.input.addEventListener('input', debounce(markupCountryResult, 500));
+let nameCountry;
+
+function markupCountryResult() {
+  nameCountry = refs.input.value;
+  fetchCountries(nameCountry).then(resultMoreTen).catch(message => pnotify(message));
+};
+
+
+
+function resultMoreTen(array) {
+  
+let numberCountries = array.length;
+ if (numberCountries === 1) {
+    const markup = countryCardTpl(array[0]);
+    refs.result.innerHTML = markup;
+  } else if (numberCountries <= 10) {
+    const markup = countriesListTpl(array);
+    refs.result.innerHTML = markup;
+  } else if (numberCountries > 10) {
+    pnotify(errorMessage);
+  } else {
+   refs.result.innerHTML = `Введена ерунда`;
+  }
+  }
+  
